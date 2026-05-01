4aeb81f0cbb9e2d8be91209744225f0ef9bf945a
Yudhajit24 <135546055+Yudhajit24@users.noreply.github.com>
2026-05-01 10:14:00 +0530

    chore: initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>WTF Insights</h1>
+        <p>Distilling wisdom from the podcast.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
